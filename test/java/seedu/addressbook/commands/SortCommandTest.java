35296d1cbc8d28e85f557947839fd3f65d2a5aeb
JiaHaoLim <jiahaolimwork@hotmail.com>
2019-02-10 19:00:25 +0800

    Added SortCommandTest.java

--- test/java/seedu/addressbook/commands/SortCommandTest.java
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -4,60 +4,45 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.Test;
 
+import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.AddressBook;
-import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.util.TypicalPersons;
 
-public class FindCommandTest {
+public class SortCommandTest {
 
     private final AddressBook addressBook = new TypicalPersons().getTypicalAddressBook();
     private final TypicalPersons td = new TypicalPersons();
 
     @Test
-    public void execute() throws IllegalValueException {
-        //same word, same case: matched
-        assertFindCommandBehavior(new String[]{"Amy"}, Arrays.asList(td.amy));
+    public void execute() {
 
-        //same word, different case: not matched
-        assertFindCommandBehavior(new String[]{"aMy"}, Collections.emptyList());
+        assertSortCommandBehavior("ascend", Arrays.asList(td.amy, td.bill, td.candy, td.dan));
+        assertSortCommandBehavior("descend", Arrays.asList(td.dan, td.candy, td.bill, td.amy));
 
-        //partial word: not matched
-        assertFindCommandBehavior(new String[]{"my"}, Collections.emptyList());
-
-        //multiple words: matched
-        assertFindCommandBehavior(new String[]{"Amy", "Bill", "Candy", "Destiny"},
-                Arrays.asList(td.amy, td.bill, td.candy));
-
-        //repeated keywords: matched
-        assertFindCommandBehavior(new String[]{"Amy", "Amy"}, Arrays.asList(td.amy));
-
-        //Keyword matching a word in address: not matched
-        assertFindCommandBehavior(new String[]{"Clementi"}, Collections.emptyList());
     }
 
     /**
      * Executes the find command for the given keywords and verifies
      * the result matches the persons in the expectedPersonList exactly.
      */
-    private void assertFindCommandBehavior(String[] keywords, List<ReadOnlyPerson> expectedPersonList) {
-        FindCommand command = createFindCommand(keywords);
+    private void assertSortCommandBehavior(String order, List<ReadOnlyPerson> expectedPersonList) {
+        SortCommand command = createSortCommand(order);
         CommandResult result = command.execute();
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        String expectedMessage = String.format(Messages.MESSAGE_PERSONS_SORTED_OVERVIEW);
 
-        assertEquals(Command.getMessageForPersonListShownSummary(expectedPersonList), result.feedbackToUser);
+        assertEquals(expectedMessage, result.feedbackToUser);
+        assertEquals(expectedPersonList, allPersons);
     }
 
-    private FindCommand createFindCommand(String[] keywords) {
-        final Set<String> keywordSet = new HashSet<>(Arrays.asList(keywords));
-        FindCommand command = new FindCommand(keywordSet);
+    private SortCommand createSortCommand(String order) {
+        SortCommand command = new SortCommand(order);
         command.setData(addressBook, Collections.emptyList());
         return command;
     }
-
 }
